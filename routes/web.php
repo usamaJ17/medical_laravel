ef607240ec77a35258a04700aec7fd11ea88eb93
usamaJ17 <usamajalal17@gmail.com>
2024-06-20 17:53:33 +0500

    added JS

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,3 +19,5 @@ Route::get('/', function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('dashboard',[DashboardController::class,'index'])->name('dashboard');
